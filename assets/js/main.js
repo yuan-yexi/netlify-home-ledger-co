89dc2650ac6d9c3e5ce6bcf6d73ef817312c01f3
yuan-yexi <blaizh@gmail.com>
2020-07-02 15:24:32 +0800

    added agent section and updated timeline section with new layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,16 @@
       });
     }
   });
+  // Smooth anchor transition
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
 
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
